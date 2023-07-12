a390fe83bdd87f43ca1ea6bf853608a1509ffa3f
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-07-12 16:49:19 -0600

    validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
     belongs_to :organizer, class_name: "User"
     has_many :attendances
     has_many :attendees, through: :attendances, source: :user
+
+    validates :name, :date, :location, presence: true
 end
