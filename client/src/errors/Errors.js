036264701cf5babdb1efd298a77d3977cb87222c
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-07-13 13:13:26 -0600

    usercontext, errorcontext, and error.js created

---
+++ client/src/errors/Errors.js
@@ -0,0 +1,12 @@
+import { useContext } from "react";
+import { ErrorContext } from "../context/ErrorContext";
+
+function Errors() {
+    const {errors} = useContext(ErrorContext);
+    const errorList = errors.map((error, idx) => <p key={idx}>{error}</p>)
+    return (
+        <div className="errors">{errorList}</div>
+    )
+}
+
+export default Errors;
