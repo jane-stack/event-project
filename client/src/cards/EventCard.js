61e21ff71c4d156e9728b72f44f631863975f2e4
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-07-13 20:11:36 -0600

    event can be deleted only by the organizer

--- client/src/cards/EventCard.js
+++ client/src/cards/EventCard.js
@@ -1,6 +1,19 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
+import { EventContext } from "../context/EventContext";
 
 function EventCard ({ event }) {
+    const { user } = useContext(UserContext);
+    const { deleteEvent } = useContext(EventContext);
+
+    const onDeleteEvent = () => {
+        fetch(`/events/${event.id}`, {
+            method: "DELETE",
+        })
+        .then(resp => resp.json())
+        .then(deleteEvent(event.id))
+    }
     
     return (
         <div>
@@ -9,7 +22,12 @@ function EventCard ({ event }) {
                 <td>{event.date}</td>
                 <td>{event.location}</td>
                 <td>
-                    <button><NavLink to={`/events/${event.id}`}>Detail</NavLink></button> 
+                    <button><NavLink to={`/events/${event.id}`}>Detail</NavLink></button>
+                    {user && user.name === event.organizer?.name && (
+                        <>
+                        <button onClick={onDeleteEvent}>Delete</button>
+                        </>
+                    )} 
                 </td>
             </tbody>
         </div>
